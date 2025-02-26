d24221a51c0ef46cbb5ba714a9426a896e09183e
Sebastian Christensen <sebc8@hotmail.com>
2025-02-26 15:43:25 +0100

    initialize project structure with essential files and configurations

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    //
+    public function index()
+    {
+        return Contact::all();
+    }
+}
